383ebdf4f4d43eadc0f2faeb71d42a525c9a3ceb
Charlie <c.thurlow.ct@googlemail.com>
2023-01-17 11:29:54 +0000

    goats id

--- server/app.js
+++ server/app.js
@@ -4,10 +4,32 @@ const express = require("express"); //access to the express library
 
 const app = express(); //mkae a basic server using express
 
+const { goats, nextId } = require("./goats")
+
 //tell the app what kind of requests to listen for
 
 app.get("/", (req, res) => {
-    res.send("Hello, world!");
+    res.json({
+        "message": "Welcome ot the GOAT API!"
+    })
+})
+
+app.get("/goats", (req, res) => {
+    res.json(goats)
+})
+
+app.get("/goats/:id", (req, res) => {
+
+    
+    const id = req.params["id"];
+
+    //filter the goat list for the relevant goat
+    console.log(goats);
+    const goat = goats.filter(g => g["id"] == id)[0];
+    goat ? res.json(goat) : res.status(404).json({ error: "No such Goat!" });  //send a status of 404 if no goats are sound
+    console.log(goat);
+
+    
 })
 
 module.exports = app;
